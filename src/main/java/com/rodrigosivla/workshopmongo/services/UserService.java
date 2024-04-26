187c3a50b0ae23587e5d676b234a7c282e1871b5
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-26 11:27:57 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/rodrigosivla/workshopmongo/services/UserService.java
+++ src/main/java/com/rodrigosivla/workshopmongo/services/UserService.java
@@ -31,8 +31,12 @@ public class UserService {
         return repo.save(obj);
     }
 
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
-
 }
