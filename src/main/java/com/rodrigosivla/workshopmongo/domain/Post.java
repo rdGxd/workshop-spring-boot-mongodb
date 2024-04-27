e49525c6a775edd4fe10fccf040ecb38b53a33b6
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-27 09:37:16 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/rodrigosivla/workshopmongo/domain/Post.java
+++ src/main/java/com/rodrigosivla/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.rodrigosivla.workshopmongo.domain;
 
 import com.rodrigosivla.workshopmongo.dto.AuthorDTO;
+import com.rodrigosivla.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,6 +20,7 @@ public class Post implements Serializable {
     private String body;
 
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -27,6 +31,14 @@ public class Post implements Serializable {
         this.title = title;
         this.body = body;
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     public String getId() {
