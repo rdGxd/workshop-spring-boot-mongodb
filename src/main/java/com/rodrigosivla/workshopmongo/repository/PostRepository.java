4a379c9c8136b93a9a0147d2b5c1c8b11a224505
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-27 10:28:09 -0300

    Consulta simples com @Query

--- src/main/java/com/rodrigosivla/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rodrigosivla/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.rodrigosivla.workshopmongo.repository;
 
 import com.rodrigosivla.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String author);
+    
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
